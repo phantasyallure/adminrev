aa45d4735761606e1589cad728bb54796b97c588
Sam ell1 <samell072003@gmail.com>
2026-08-12 07:39:38 +0200

    Add Suggestions route to App component

--- reeviewit-admin/src/App.jsx
+++ reeviewit-admin/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Reviews from './pages/Reviews'
 import Places from './pages/Places'
+import Suggestions from './pages/Suggestions'
 import Users from './pages/Users'
 import Badges from './pages/Badges'
 import Roles from './pages/Roles'
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/reviews" element={<ProtectedRoute requires="can_approve_reviews"><Reviews /></ProtectedRoute>} />
         <Route path="/places" element={<ProtectedRoute requires="can_manage_places"><Places /></ProtectedRoute>} />
+        <Route path="/suggestions" element={<ProtectedRoute requires="can_manage_places"><Suggestions /></ProtectedRoute>} />
         <Route path="/users" element={<ProtectedRoute requires="can_ban_users"><Users /></ProtectedRoute>} />
         <Route path="/badges" element={<ProtectedRoute requires="can_award_badges"><Badges /></ProtectedRoute>} />
         <Route path="/roles" element={<ProtectedRoute requires="can_manage_roles"><Roles /></ProtectedRoute>} />
