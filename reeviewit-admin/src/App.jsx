0647098bc45712b2cf80a0a0adb30de6f22d34c0
Sam ell1 <samell072003@gmail.com>
2026-08-15 10:02:54 +0200

    Add ProductPosts route to App component

--- reeviewit-admin/src/App.jsx
+++ reeviewit-admin/src/App.jsx
@@ -4,6 +4,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Reviews from './pages/Reviews'
+import ProductPosts from './pages/ProductPosts'
 import Places from './pages/Places'
 import Suggestions from './pages/Suggestions'
 import Users from './pages/Users'
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/reviews" element={<ProtectedRoute requires="can_approve_reviews"><Reviews /></ProtectedRoute>} />
+        <Route path="/products" element={<ProtectedRoute requires="can_approve_reviews"><ProductPosts /></ProtectedRoute>} />
         <Route path="/places" element={<ProtectedRoute requires="can_manage_places"><Places /></ProtectedRoute>} />
         <Route path="/suggestions" element={<ProtectedRoute requires="can_manage_places"><Suggestions /></ProtectedRoute>} />
         <Route path="/users" element={<ProtectedRoute requires="can_ban_users"><Users /></ProtectedRoute>} />
