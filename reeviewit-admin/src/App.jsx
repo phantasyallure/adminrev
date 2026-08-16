b31cdc6a4617d4c64e1ddcfe4abddddafd85b8d1
Sam ell1 <samell072003@gmail.com>
2026-08-16 06:22:21 +0200

    Add Import route to App component

--- reeviewit-admin/src/App.jsx
+++ reeviewit-admin/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Reviews from './pages/Reviews'
 import ProductPosts from './pages/ProductPosts'
 import Places from './pages/Places'
+import Import from './pages/Import'
 import Suggestions from './pages/Suggestions'
 import Users from './pages/Users'
 import Badges from './pages/Badges'
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/reviews" element={<ProtectedRoute requires="can_approve_reviews"><Reviews /></ProtectedRoute>} />
         <Route path="/products" element={<ProtectedRoute requires="can_approve_reviews"><ProductPosts /></ProtectedRoute>} />
         <Route path="/places" element={<ProtectedRoute requires="can_manage_places"><Places /></ProtectedRoute>} />
+        <Route path="/import" element={<ProtectedRoute requires="can_manage_places"><Import /></ProtectedRoute>} />
         <Route path="/suggestions" element={<ProtectedRoute requires="can_manage_places"><Suggestions /></ProtectedRoute>} />
         <Route path="/users" element={<ProtectedRoute requires="can_ban_users"><Users /></ProtectedRoute>} />
         <Route path="/badges" element={<ProtectedRoute requires="can_award_badges"><Badges /></ProtectedRoute>} />
